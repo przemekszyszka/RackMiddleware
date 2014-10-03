354af62e1bbf3707135d7cba172a81eab4e31ae6
Przemysław Szyszka <przemeklo@o2.pl>
2014-10-03 16:47:37 +0200

    #4 Change call method to return 429 status when possible number of requests is exceeded

--- szysza_middleware/lib/middleware.rb
+++ szysza_middleware/lib/middleware.rb
@@ -6,10 +6,14 @@ class Middleware
   end
 
   def call(env)
-    @remaining -= 1
-    response = @app.call(env)
-    response[1]["X-RateLimit-Limit"] = @options[:limit]
-    response[1]["X-RateLimit-Remaining"] = @remaining
-    response
+    if @remaining > 0
+      @remaining -= 1
+      response = @app.call(env)
+      response[1]["X-RateLimit-Limit"] = @options[:limit]
+      response[1]["X-RateLimit-Remaining"] = @remaining
+      response
+    else
+      Rack::MockResponse.new(429, { "Content-Type" => "text-html" }, "Too Many Requests")
+    end
   end
 end
