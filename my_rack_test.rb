7c23ff9cd545d79edd894ff040e9cb3653035985
Przemysław Szyszka <przemeklo@o2.pl>
2014-10-03 14:28:10 +0200

    #2 Create tests for middleware

--- my_rack_test.rb
+++ my_rack_test.rb
@@ -2,6 +2,7 @@ require "rubygems"
 require "rack/test"
 require 'minitest/autorun'
 require './app.rb'
+require './middleware.rb'
 
 
 class MyRackTest < Minitest::Test
@@ -9,10 +10,18 @@ class MyRackTest < Minitest::Test
 
   def setup
     @app = App.new
-    @middleware = Middleware.new(@app)
+    @middleware = Middleware.new(@app, { "limit" => 60 })
   end
 
   def test_app_returns_an_response
-    assert_equal ["200", {"Content-Type" => "text-html"}, ["Test \n"]], @app.call("Test env")
+    assert_equal ["200", {"Content-Type" => "text-html"}, ["Test \n"]], @app.call({})
+  end
+
+  def test_header_has_x_retelimit_limit_in
+    assert @middleware.call({})[1].has_key?("X-RateLimit-Limit")
+  end
+
+  def test_x_retelimit_limit_header_has_value_specified
+    assert_equal 60, @middleware.call({})[1]["X-RateLimit-Limit"]
   end
 end
