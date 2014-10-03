80d0a6ec9880cc364f7de11351a5f33f82d92613
Przemysław Szyszka <przemeklo@o2.pl>
2014-10-03 14:46:04 +0200

    #3 Create middleware

--- middleware.rb
+++ middleware.rb
@@ -2,11 +2,14 @@ class Middleware
   def initialize(app, options)
     @app = app
     @options = options
+    @remaining = options["limit"]
   end
 
   def call(env)
+    @remaining -= 1
     response = @app.call(env)
     response[1]["X-RateLimit-Limit"] = @options["limit"]
+    response[1]["X-RateLimit-Remaining"] = @remaining
     response
   end
 end
