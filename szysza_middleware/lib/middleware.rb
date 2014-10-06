44f72e61c48a61aa3752e8ca1e7a29023a5fef43
Przemysław Szyszka <przemeklo@o2.pl>
2014-10-06 09:29:18 +0200

    #5 Update middleware to support limit reset

--- szysza_middleware/lib/middleware.rb
+++ szysza_middleware/lib/middleware.rb
@@ -3,14 +3,20 @@ class Middleware
     @app = app
     @options = options
     @remaining = options[:limit]
+    @reset_at = Time.now + options[:reset_in]
   end
 
   def call(env)
+    if @reset_at - Time.now < 0
+      @remaining = @options[:limit]
+      @reset_at += @options[:reset_in]
+    end
     if @remaining > 0
       @remaining -= 1
       response = @app.call(env)
-      response[1]["X-RateLimit-Limit"] = @options[:limit]
+      response[1]["X-RateLimit-Limit"]     = @options[:limit]
       response[1]["X-RateLimit-Remaining"] = @remaining
+      response[1]["X-RateLimit-Reset"]     = @reset_at
       response
     else
       Rack::MockResponse.new(429, { "Content-Type" => "text-html" }, "Too Many Requests")
